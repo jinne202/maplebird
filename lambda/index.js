6f7042d3357db216d48c08e475098fe1450f5542
jinne202 <hanb5468@gmail.com>
2020-08-12 20:04:45 +0900

    lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,41 +1,38 @@
 const AWS = require('aws-sdk');
 const Sharp = require('sharp');
 
-const S3 = new AWS.S3({
-    region : 'ap-northeast-2'
-});
+const S3 = new AWS.S3({ region: 'ap-northeast-2' });
 
-exports.handler = async (evnet, context, callback) => {
-    const Bucket = event.Records[0].s3.bucket.name;
-    const Key = event.Records[0].s3.object.key;
-    const filename = Key.split('/')[Key.split('/').length - 1];
-    const ext = Key.split('.')[Key.split('.').length - 1];
-    console.log(Bucket, Key, filename, ext);
-    const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
-
-    try {
-        const s3Object = await S3.getObject({
-            Bucket,
-            key,
-        }).promise();
-        console.log('original', s3Object.Body.length);
-
-        const resizeImage = await Sharp(s3Object.Body)
-        .resize(800,800, {
-            fit : 'inside'
-        })
-        .toFormat(requiredFormat)
-        .toBuffer();
-
-        await s3.putObject({
-            Bucket,
-            Key : `thumb/${filename}`,
-            Body : resizeImage,
-        }).promise();
-        console.log('put');
-        return callback(null, `thumb/${filename}`);
-    } catch (e) {
-        console.error(e);
-        return callback(e);
-    }
-}
+exports.handler = async (event, context, callback) => {
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = event.Records[0].s3.object.key;
+  const filename = Key.split('/')[Key.split('/').length - 1];
+  const ext = Key.split('.')[Key.split('.').length - 1];
+  console.log(Key, filename, ext);
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+  
+  try {
+    const s3Object = await S3.getObject({
+      Bucket,
+      Key,
+    }).promise();
+    console.log('original', s3Object.Body.length);
+    const resizedImage = await Sharp(s3Object.Body)
+      .resize(800, 800, {
+        fit: 'inside',
+      })
+      .toFormat(requiredFormat)
+      .toBuffer();
+    console.log('resize', resizedImage.length);
+    await S3.putObject({
+      Body: resizedImage,
+      Bucket,
+      Key: `thumb/${filename}`,
+    }).promise();
+    console.log('put');
+    return callback(null, `thumb/${filename}`);
+  } catch (error) {
+    console.error(error);
+    return callback(error);
+  }
+};
